35d2176d39d57ddf5f7e342f82ed7451169f569c
João Victor Carvalho <joaovictor.crvl@gmail.com>
2026-03-30 19:52:14 -0300

    ajuste tamanho imagem

--- src/ImageViewer.java
+++ src/ImageViewer.java
@@ -13,6 +13,6 @@ public class ImageViewer extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawImage(image, 0, 0, null);
+        g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
     }
 }
